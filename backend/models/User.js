895ff32dccfec5186035caef7889ca47953186e6
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-01-24 15:15:16 -0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     role: {
         type: String,
         enum: ["admin", "resident"],
@@ -11,4 +17,4 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
