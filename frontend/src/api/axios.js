0c0a4711318f9b3a04872a6b3dfd8f2be7ca2def
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-01-26 14:27:01 -0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
+const API = "https://tagt.onrender.com";
+
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: API,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default api;
