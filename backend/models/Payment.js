e4889bb41c32c32b2113caac128e538b550ab077
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-01-26 13:21:39 -0500

    Update Payment.js

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,12 +1,11 @@
-const mongoose = require("mongoose");
-
 const PaymentSchema = new mongoose.Schema({
     residentId: mongoose.Schema.Types.ObjectId,
     month: String,
+    amount: Number,
     status: {
         type: String,
-        enum: ["PAID", "DUE"],
-        default: "DUE"
+        enum: ["paid", "unpaid"],
+        default: "unpaid"
     }
 });
 
