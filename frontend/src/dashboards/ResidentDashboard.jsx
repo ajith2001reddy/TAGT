92fcca1e124da41f7b8e1be5857e827fd25e53e4
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-01-26 15:19:40 -0500

    Update ResidentDashboard.jsx

--- frontend/src/dashboards/ResidentDashboard.jsx
+++ frontend/src/dashboards/ResidentDashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import api from "./api";
+import api from "../api/axios";
+
 
 export default function ResidentDashboard() {
     const [payments, setPayments] = useState([]);
