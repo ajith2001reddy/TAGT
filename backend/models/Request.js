455692b2d40ebead11e19be6623ad5806dbe832e
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-01-26 13:21:43 -0500

    Update Request.js

--- backend/models/Request.js
+++ backend/models/Request.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
-const RequestSchema = new mongoose.Schema({
-    residentId: mongoose.Schema.Types.ObjectId,
-    message: String,
-    status: { type: String, default: "OPEN" }
-});
+const RequestSchema = new mongoose.Schema(
+    {
+        residentId: mongoose.Schema.Types.ObjectId,
+        message: String,
+        status: {
+            type: String,
+            enum: ["pending", "in-progress", "resolved"],
+            default: "pending"
+        }
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("Request", RequestSchema);
