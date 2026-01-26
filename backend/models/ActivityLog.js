89e4b37f735bb392f93e0e0786770085115d53fb
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-01-26 13:35:58 -0500

    Update ActivityLog.js

--- backend/models/ActivityLog.js
+++ backend/models/ActivityLog.js
@@ -1,7 +1,22 @@
-const ActivityLogSchema = new mongoose.Schema({
-    action: String,
-    performedBy: String,
-    role: String
-}, { timestamps: true });
+const mongoose = require("mongoose");
+
+const ActivityLogSchema = new mongoose.Schema(
+    {
+        action: {
+            type: String,
+            required: true
+        },
+        performedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        role: {
+            type: String,
+            required: true
+        }
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("ActivityLog", ActivityLogSchema);
