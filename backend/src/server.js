43b1849424a0fe84f6178546a0a7b5456e8ffb46
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-03-06 12:24:25 -0500

    Remove DNS server setup from server.js
    Removed DNS server configuration from server.js.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,6 @@
 ﻿import "dotenv/config";  // MUST BE FIRST
 
-import dns from "dns";
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-dns.setDefaultResultOrder("ipv4first");
+
 
 import http from "http";
 import app from "./app.js";
